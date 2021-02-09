1b27fed608c4c90866b5815f9a7f4de8eb2b67d1
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-09 16:56:17 +0100

    Add GridView Items
    Items from dummy_data.dart are mapped into a list for the GridView, and this mapping generates a CategoryItem for any of these categories

---
+++ lib/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+          .map(
+            (dummyCategory) =>
+                CategoryItem.name(dummyCategory.title, dummyCategory.color),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
+
+//7.3
