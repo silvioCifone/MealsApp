2f0237643d97ca044e1ca0fe9050beca5225924a
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-10 18:35:24 +0100

    Add initialRoute Definition
    It permits to define a first route where the app start, that is different from "/"

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      initialRoute: "/", //It defines the default route at start
       routes: {
         //"/": (context) => CategoriesScreen(), It's the same sintax of the home parameter in MaterialApp widget arguments
         "/category-meals": (context) => CategoryMealsScreen(),
