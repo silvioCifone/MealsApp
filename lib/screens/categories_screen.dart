723026835aa89e7361cf8db884fea30a0469b662
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-16 11:48:44 +0100

    Add tabs_screen.dart
    It permits to have a tab bar above the appbar, and this tab bar has 2 tabs, categories and favorites

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,24 +5,19 @@ import 'package:meals_app/widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome Cook!"),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map(
-              (dummyCategory) => CategoryItem.name(
-                  dummyCategory.id, dummyCategory.title, dummyCategory.color),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (dummyCategory) => CategoryItem.name(
+                dummyCategory.id, dummyCategory.title, dummyCategory.color),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
